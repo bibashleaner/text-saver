a9e3b878238b0b5a9fb6a088bd13c5344efe0a23
bibashlearner <bibashchaudhary330@gmail.com>
2024-12-24 00:33:07 +0545

    added choosing font style in a text box and also added function where we can signature and download image of signature

--- src/components/SignatureBox.jsx
+++ src/components/SignatureBox.jsx
@@ -1,5 +1,6 @@
 import React, { useRef,useState } from "react";
 import SignatureCanvas from 'react-signature-canvas';
+import '../assets/css/components/signaturebox.css';
 
 export default function SignaturePad(){
     const sigCanvas = useRef(null);
@@ -8,17 +9,34 @@ export default function SignaturePad(){
         sigCanvas.current.clear();
     }
 
+    const sigSave = () =>{
+        const signatureImage = sigCanvas.current.toDataURL('image/png');
+        const a = document.createElement('a');
+        a.href = signatureImage;
+        a.download = 'signatureImage.png';
+        a.click();
+    }
+
     return (
-        <div>
+        <>
+        <div className="sign">
             <h1>Signature Box</h1>
             <SignatureCanvas
                 rows='10'
                 cols='80'
                 placeholder='Sign Here'
-                ref = {SignCanvas}
+                ref = {sigCanvas}
                 onChange={(e) => setText(e.target.value)}
-            ></SignatureCanvas>
+                canvasProps={{width: 500, height: 300, className: 'drawing-canvas',
+                    style : {border :'2px solid black'},
+                }}
+                />
         </div>
+        <div className="buttons">
+            <button onClick={clearpad}>Clear</button>
+            <button onClick={sigSave}>Save</button>
+        </div>
+        </>
     );
 
 }
